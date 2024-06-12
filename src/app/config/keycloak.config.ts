2584b51b3fbdabc4ef7e8ffa09f9f2836cae9e3a
ziad shrakey <zshrakey@gmail.com>
2024-06-12 16:53:35 +0300

    remove not use file

--- src/app/config/keycloak.config.ts
+++
@@ -1,9 +0,0 @@
-import { KeycloakConfig } from 'keycloak-angular';
-
-const keycloakConfig: KeycloakConfig = {
-  url: 'http://localhost:8080/auth', // Keycloak server URL
-  realm: 'your-realm',               // Realm name
-  clientId: 'my-angular-app',        // Client ID
-};
-
-export default keycloakConfig;
