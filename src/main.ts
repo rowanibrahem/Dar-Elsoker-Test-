e3d2fdb836ad42e78cf4ded387fcaeec716ea72a
Rowan Ibrahem <rowanibrahem2003@gmail.com>
2024-07-21 00:24:36 +0200

    add logrocket package

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,11 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+import LogRocket from 'logrocket';
+
+// Initialize LogRocket
+LogRocket.init('puvfes/mega-dev');
+
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err),
 );
